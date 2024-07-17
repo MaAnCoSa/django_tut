7f9479f7be0a8729d7df6f50ac37af523eb13d29
manuel.cota <cota.manuel@rocketmail.com>
2024-07-16 20:57:42 -0700

    Added pages app

--- src/products/models.py
+++ src/products/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default="This is cool!")
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(max_digits=10, decimal_places=2)
+    summary     = models.TextField(blank=True, null=True)
+    featured    = models.BooleanField()
 
     def __str__(self):
         return self.title
